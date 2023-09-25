78444145c1cef810c0cc2049bc56fad4af10db6e
Altalya Solutions <systems@altalya.com>
2023-09-25 15:19:16 +0530

    basic

--- basics/while.py
+++ basics/while.py
@@ -1,5 +1,11 @@
 num =int(input("enter the number"))
-i=0
-while i<num:
-    print(i)
-    i+=1
+def print1(num):
+    i=0
+    while i<num:
+        print(i)
+        i+=1
+print1(num)
+num1 =int(input("enter the number"))
+print1(num1)
+num2 =int(input("enter the number"))
+print1(num2)
